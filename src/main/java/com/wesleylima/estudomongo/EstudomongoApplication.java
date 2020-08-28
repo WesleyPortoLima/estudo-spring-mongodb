5e91a29c03fd2c8f0c4db651153f1b917c96070b
Wesley Lima <wesleylima2209@gmail.com>
2020-08-28 06:52:55 -0300

    Criação do projeto

---
+++ src/main/java/com/wesleylima/estudomongo/EstudomongoApplication.java
@@ -0,0 +1,13 @@
+package com.wesleylima.estudomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesleylima/estudomongo/EstudomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleylima.estudomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
